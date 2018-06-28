f9ea01ef44c68071df6572683818da17b2840ab8
Juan Perea <jpereac@gmail.com>
2018-06-28 20:33:12 +0200

    ws

--- src/main/java/edu/uclm/esi/disoft/comandas/ws/ServidorWS.java
+++ src/main/java/edu/uclm/esi/disoft/comandas/ws/ServidorWS.java
@@ -1,48 +1,50 @@
 package edu.uclm.esi.disoft.comandas.ws;
 
 import java.util.Enumeration;
-import java.util.Vector;
-//import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentHashMap;
 
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import edu.uclm.esi.disoft.comandas.dominio.Manager;
 
-@ServerEndpoint(value = "/ServidorWS")
+@ServerEndpoint(value="/ServidorWSComandas")
 public class ServidorWS {
-	private static Vector<Session> sessions=new Vector<>();
-	//private static ConcurrentHashMap<String, Session> sessions = new ConcurrentHashMap<>();
-	
-	private static class WSHolder{
-		static ServidorWS singleton=new ServidorWS();
-	}
-	
-	public static ServidorWS get() {
-		return WSHolder.singleton;
-	}
+	private static ConcurrentHashMap<String, Session> sessions = new ConcurrentHashMap<>();
 	
 	@OnOpen
 	public void onOpen(Session session) {
-		sessions.add(session);
+		sessions.put(session.getId(), session);
 	}
-
+	
 	@OnMessage
-	public void onMessage(Session session, String mensaje) throws Exception {
+	public void onMessage(Session session, String mensaje) {
 		JSONObject jso = new JSONObject(mensaje);
-		Manager.get().platoPreparado(jso);
-		session.getAsyncRemote().sendText(mensaje);
+		String type = jso.getString("type");
+		switch (type) {
+		case "PlatoFinalizado":
+			String plato=jso.getString("plato");
+			int idMesa = jso.getInt("idMesa");
+			Manager.get().platoFinalizado(plato, idMesa);
+			break;
+		}
 	}
 
-	public static void send(JSONObject jso) {
+	public static void solicitarPlatos(JSONArray platos,int idMesa) {
+		JSONObject mensaje = new JSONObject();
+		mensaje.put("type","solicitudDePlatos");
+		mensaje.put("idMesa", idMesa);
+		mensaje.put("platos", platos);
 		Enumeration<Session> sesiones = sessions.elements();
-		while (sesiones.hasMoreElements()) {
+		while(sesiones.hasMoreElements()) {
 			Session sesion = sesiones.nextElement();
-			sesion.getAsyncRemote().sendText(jso.toString());
+			if (sesion.isOpen())
+				sesion.getAsyncRemote().sendText(mensaje.toString());// Cuando hago este send es cuando se ejecuta el onmessage
 		}
 	}
 }
